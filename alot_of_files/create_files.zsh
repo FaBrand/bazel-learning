850e0d05fdea2114855e9c00310fdd860015c7d9
21087362+FaBrand@users.noreply.github.com <21087362+FaBrand@users.noreply.github.com>
2019-04-04 18:48:29 +0200

    Account for the 0 case with the deps

--- alot_of_files/create_files.zsh
+++ alot_of_files/create_files.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 git clean -xfd
-n=10000
+n=1000
 mkdir target_{1..$n}
 touch target_{1..$n}/BUILD
 touch target_{1..$n}/source_file.cpp
@@ -9,6 +9,8 @@ touch target_{1..$n}/source_file.h
 cat ./BUILD.template > target_{1..$n}/BUILD
 cat ./source_file.cpp.template > target_{1..$n}/source_file.cpp
 cat ./source_file.h.template > target_{1..$n}/source_file.h
+
+sed -i "/<DEP>/d" target_1/BUILD
 
 for n in {2..$n}; do
     (( x=$n - 1 ))
