898098a9613f509735056f900509fdb0a397bf38
21087362+FaBrand@users.noreply.github.com <21087362+FaBrand@users.noreply.github.com>
2019-04-04 18:41:45 +0200

    Add dependencies

--- alot_of_files/create_files.zsh
+++ alot_of_files/create_files.zsh
@@ -10,6 +10,11 @@ cat ./BUILD.template > target_{1..$n}/BUILD
 cat ./source_file.cpp.template > target_{1..$n}/source_file.cpp
 cat ./source_file.h.template > target_{1..$n}/source_file.h
 
+for n in {2..$n}; do
+    (( x=$n - 1 ))
+    sed -i "s#<DEP>#//target_${x}:a_lib#g" target_${n}/BUILD
+done
+
 mkdir /tmp/profile 2>&1 > /dev/null
 
 bazel build --nobuild --profile=/tmp/profile/no-build-profile //...
