fde4cf327f59a38611cde7f5175f197c6b017f51
21087362+FaBrand@users.noreply.github.com <21087362+FaBrand@users.noreply.github.com>
2020-12-08 10:22:49 +0100

    Print input args as well
    Issue: none

--- run_with_files/binary.py
+++ run_with_files/binary.py
@@ -8,5 +8,8 @@ from rules_python.python.runfiles import runfiles
 if __name__ == "__main__":
     r = runfiles.Create()
     for arg in sys.argv[2:]:
+        print("arg", arg)
+        print("argv1", sys.argv[1])
+        print("Rlocation", r.Rlocation(sys.argv[1]+'/'+arg))
         p_arg = Path(r.Rlocation(sys.argv[1]+'/'+arg))
         print(p_arg, p_arg.exists())
